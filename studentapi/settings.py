cb995afd9d79aec6731ee2da27e6e409221baa1d
rushida <rushida0502@gmail.com>
2024-11-21 12:58:48 +0530

    commited

--- studentapi/settings.py
+++ studentapi/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 
 from pathlib import Path
 import os
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -84,14 +85,15 @@ WSGI_APPLICATION = 'studentapi.wsgi.application'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'studentapi_db',
-        'USER':'postgres',
-        'PASSWORD':'alpha',
-        'HOST':'localhost',
-        'PORT':'5432'
-    }
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.postgresql',
+    #     'NAME': 'studentapi_db',
+    #     'USER':'postgres',
+    #     'PASSWORD':'alpha',
+    #     'HOST':'localhost',
+    #     'PORT':'5432'
+    # }
+    "default":dj_database_url.config(default='postgresql://product_management_uvp4_user:5hF13Jd4YrNQVJSFKusBrwSNsfCwVNRm@dpg-csu44ga3esus73c6urjg-a.oregon-postgres.render.com/product_management_uvp4')
 }
 
 
